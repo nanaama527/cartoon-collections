689bf7ca98f3e7d8e672e175e0d9aaf7308efcf3
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-14 15:02:20 -0500

    Update cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,22 +1,28 @@
 def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
-    p "#{index + 1}. #{dwarf}"
+    puts "#{index + 1}. #{dwarf}"
   end 
 end
 
 def summon_captain_planet(array)
-  valid_calls = ["fire", "wind", "earth", "heart"]
-  valid_calls.each do |element|
+  array.collect do |element|
     "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls(assorted_words)
-  assorted_words.any? do |word|
-    word.length.any? > 4
+  if assorted_words.any? do |word|
+    word.length > 4
+  end
     return true
+  else
+   return false
   end
 end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|cheese| cheese_types.include?(cheese)}
 end
+
+  
